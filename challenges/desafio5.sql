a34256a46e482d82f021d6e91100795106a8a145
lindainesv <lindainesvicente@hotmail.com.com>
2023-05-10 12:08:22 -0300

    ajustando formatacao desafio 5

--- challenges/desafio5.sql
+++ challenges/desafio5.sql
@@ -1,9 +1,10 @@
-SELECT nome_cancoes AS cancao,
-	COUNT(historico.id_cancao) AS reproducoes
+SELECT 
+    nome_cancoes AS cancao,
+    COUNT(historico.id_cancao) AS reproducoes
 FROM
     SpotifyClone.historico
-INNER JOIN cancoes
-ON cancoes.id_cancao = historico.id_cancao
+        INNER JOIN
+    cancoes ON cancoes.id_cancao = historico.id_cancao
 GROUP BY nome_cancoes
-ORDER BY reproducoes DESC, nome_cancoes
+ORDER BY reproducoes DESC , nome_cancoes
 LIMIT 2;
